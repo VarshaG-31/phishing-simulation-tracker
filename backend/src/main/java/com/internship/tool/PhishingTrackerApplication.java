dc3ef2107d8d49bf4c554ad02af0d1c39634ddeb
Varsha <gopalkrishna1171971>
2026-05-07 00:20:46 +0530

    Day 1 - Setup complete: Java 17, Spring Boot folders, and successful build

---
+++ backend/src/main/java/com/internship/tool/PhishingTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.internship.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhishingTrackerApplication.class, args);
+    }
+}
