6eb9a3acf22c590c049420b49b1db4dfda4bf68a
Varsha <gopalkrishna1171971>
2026-05-12 15:54:38 +0530

    Day 4: Completed REST Controller, JPA Auditing, and PostgreSQL connection

--- backend/src/main/java/com/internship/tool/PhishingTrackerApplication.java
+++ backend/src/main/java/com/internship/tool/PhishingTrackerApplication.java
@@ -2,8 +2,10 @@ package com.internship.tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Add this line!
 public class PhishingTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PhishingTrackerApplication.class, args);
